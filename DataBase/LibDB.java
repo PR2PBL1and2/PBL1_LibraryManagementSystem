61f126230edc9edf547ccd1e27fc7dc2d0896eb9
kyokwan <ahnkk0904@naver.com>
2025-10-17 11:56:23 +0900

    1

--- DataBase/LibDB.java
+++ DataBase/LibDB.java
@@ -1,62 +1,39 @@
 package DataBase;
+
 import java.util.*;
 import myClass.DB_Element;
 
 /**
- * LibDB 클래스의 설명을 작성하세요.
+ * App 클래스의 설명을 작성하세요.
  *
- * @author (작성자 이름)
- * @version (버전 번호 또는 작성한 날짜)
+ * @author (2021320014_안교관)
+ * @version (2025.10.16)
  */
-public class LibDB<T extends DB_Element>
-{
+public class LibDB<T extends DB_Element> {
     private ArrayList<T> db;
-
-    /**
-     * LibDB 클래스의 객체 생성자
-     */
-    public LibDB()
-    {
-        this.db = new ArrayList<T>();
+    
+    public LibDB() {
+        this.db = new ArrayList<>();
     }
-
-    /**
-     * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
-     *
-     * @param  y  메소드의 샘플 파라미터
-     * @return    x 더하기 y의 결과값을 반환
-     */
-    public void addElement(T element)
-    {
+    
+    public void addElement(T element) {
         db.add(element);
     }
-
-    /**
-     * 메소드 예제 - 사용자에 맞게 주석을 바꾸십시오.
-     *
-     * @param  y  메소드의 샘플 파라미터
-     * @return    x 와 y의 합
-     */
-    public T findElement(String id)
-    {
+    
+    public T findElement(String id) {
         Iterator<T> it = db.iterator();
-        while(it.hasNext()){
-            
+        while (it.hasNext()) {
+            T elem = it.next();
+            if (elem != null && elem.getID() != null && elem.getID().equals(id)) {
+                return elem;
+            }
+        }
+        return null;
+    }
+    
+    public void printAllElements() {
+        for (T element : db) {
+            System.out.println(element);
         }
     }
-
-    /**
-     * 메소드 예제 - 사용자에 맞게 주석을 바꾸십시오.
-     *
-     * @param  y  메소드의 샘플 파라미터
-     * @return    x 와 y의 합
-     */
-    public void printAllElement()
-    {
-        for (T element : db){
-            
-        }
-    }
-
-
 }
