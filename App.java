73ef4449db8e4f82711701c94eed4213217f9a80
gihong00627 <gihonglim0627@gmail.com>
2025-10-17 19:23:00 +0900

    최신화

--- App.java
+++ App.java
@@ -12,29 +12,30 @@ public class App
 {
     public static void main(String[] args){
         // 1
-        LibraryManagementSystem lbSystem = new LibraryManagementSystem();
+        LibraryManagementSystem lms = new LibraryManagementSystem();
         
         // 2
-        lbSystem.setUserDB("C:\\Temp\\UserData2025.txt");
+        lms.setUserDB("C:\\Temp\\UserData2025.txt");
         
         // 3
         System.out.println("----- 이용자 목록 출력 -----");
-        lbSystem.printDB(lbSystem.userDB);
+        lms.printDB(lms.userDB);
         
         // 4
-        lbSystem.setBookDB("C:\\Temp\\BookData2025.txt");
+        lms.setBookDB("C:\\Temp\\BookData2025.txt");
         
         // 5
-        System.out.println("----- 책 목록 출력 -----");
-        lbSystem.printDB(lbSystem.bookDB);
+        System.out.println("\n----- 책 목록 출력 -----");
+        lms.printDB(lms.bookDB);
         
         // 6
-        lbSystem.borrowBook("2023320003", "B04");
-        lbSystem.borrowBook("2025320001", "B02");
-        lbSystem.borrowBook("2024320002", "B03");
+        lms.borrowBook("2023320003", "B04");
+        lms.borrowBook("2025320001", "B02");
+        lms.borrowBook("2024320002", "B03");
         
         // 7
-        System.out.println("----- 대출 현황 -----");
-        lbSystem.printLoanList();
+        System.out.println("\n----- 대출 현황 -----");
+        lms.printLoanList();
+        System.out.println("--------------------\n");
     }
 }
